10a14f482261caa3b27f3f13bd42e6edd6ed90e0
ebasthe <ebasthe@gmail.com>
2026-01-24 19:47:46 +0200

    Add Google Analytics tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JH0W21QV96"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-JH0W21QV96');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
